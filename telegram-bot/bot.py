c0cc83c2cfde6faf47fbfd4373bdd2dad6ec047c
Kirill <vadimvolodickij@gmail.com>
2026-02-15 11:14:53 +0100

    Add basic Telegram bot with /start command

---
+++ telegram-bot/bot.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import Application, CommandHandler
+
+load_dotenv()  # загружает переменные из .env(downloads variables from .env)
+TOKEN = os.getenv('TOKEN')  # Getting a Token
+
+# parametrs update and context contain info about event (Handlers always get these params)
+async def start(update: Update, context):
+    """Command /start - greet"""
+    # Answer on the message
+    await update.message.reply_text('Hallo, ich helfe dir gern dein Ausbildung/Duales Studium zu finden')
+
+
+def main():
+    """Main function starts and sets bot"""
+    app = Application.builder().token(TOKEN).build()
+    """Register a command /start"""
+    app.add_handler(CommandHandler('start', start))
+    print('Bot gestartet!')
+    """Start listening a Bot"""
+    app.run_polling()
+
+
+if __name__ == '__main__':
+    main()
