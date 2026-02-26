1d8ce475c3e258898f96164d4b857ccfaaa17004
Kirill <vadimvolodickij@gmail.com>
2026-02-26 17:23:29 +0100

    Add initial setup for job caching in data management

--- telegram-bot/data.py
+++ telegram-bot/data.py
@@ -3,3 +3,8 @@ Data management for the bot
 Loads job listenings from parser or cache
 """
 
+from database.parser_ausbildung import parse_ausbildung_de
+from typing import Dict,List
+
+#Cache for saving results
+_job_cache = {}
